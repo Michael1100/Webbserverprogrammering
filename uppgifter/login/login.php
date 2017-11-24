cf583a03734f702c1ee1a90fb9764052d49833fc
Michael1100 <michael.gabrielsson.te16d@gmail.com>
2017-11-24 12:17:12 +0100

    Kod för login sida

---
+++ uppgifter/login/login.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html>
+	<head> 
+	<meta charset="utf-8">
+	</head>
+	<body>
+
+
+		<form method="POST" action="submit.php">
+			Användarnamn:
+				<input text="text" name="Username">
+			</br>
+			Lösenord:
+				<input text="text" name="Password">
+			</br>
+			
+			<input type="submit" value="Send">
+		</form>
+	</body>
+</html>
